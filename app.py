03eb3c405e79cd925a92753c39e13cf4d53e9c3d
BrunoSimon <bru.sim.bs@gmail.com>
2021-04-27 10:18:27 +0200

    Premiers traitements

---
+++ app.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 21 17:01:42 2021
+
+@author: Bruno
+"""
+import dash
+
+app = dash.Dash(__name__, suppress_callback_exceptions=False)
+server = app.server
---
+++ callbacks.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 21 17:01:05 2021
+
+@author: Bruno
+"""
+from dash.dependencies import Input, Output
+
+from app import app
+
+@app.callback(
+    Output('app-1-display-value', 'children'),
+    Input('app-1-dropdown', 'value'))
+def display_value(value):
+    return 'You have selected "{}"'.format(value)
+
+@app.callback(
+    Output('app-2-display-value', 'children'),
+    Input('app-2-dropdown', 'value'))
+def display_value(value):
+    return 'You have selected "{}"'.format(value)
