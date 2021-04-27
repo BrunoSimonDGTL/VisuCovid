486c5c0c13c141b301565e4a05df0cbb9ebb47ad
BrunoSimon <bru.sim.bs@gmail.com>
2021-04-27 11:47:31 +0200

    Premier graph avec Dash

--- app.py
+++ app.py
@@ -6,5 +6,7 @@ Created on Wed Apr 21 17:01:42 2021
 """
 import dash
 
+
+
 app = dash.Dash(__name__, suppress_callback_exceptions=False)
 server = app.server
--- layouts.py
+++ layouts.py
@@ -6,6 +6,37 @@ Created on Wed Apr 21 17:00:08 2021
 """
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.express as px
+
+import pandas as pd
+import os
+
+cwd = os.getcwd()
+rep ="./data/"
+filename = "data.xlsx"
+
+with pd.ExcelFile(rep+filename) as xls:
+    #print(xls.sheet_names )
+    df = pd.read_excel(xls, "Sheet2")
+
+#remove unnamed
+for k in df :
+    if "Unnamed" in k :
+        df = df.drop(columns=[k])
+# Convert to timedata
+df["date"] = pd.to_datetime(df["date"])
+
+# Replace missing count with 0
+df['total_cases'] = df['total_cases'].fillna(0)
+df['total_deaths'] = df['total_deaths'].fillna(0)
+
+#convert count to int
+df['total_cases'] = df['total_cases'].astype(int)
+df['total_deaths'] = df['total_deaths'].astype(int)
+
+dfFR = df[df.iso_code == 'FRA']
+
+fig = px.line(dfFR, x="date", y="total_cases", title='Number of cases in France')
 
 layout0 = html.Div([
     html.H3('Root'),
@@ -27,7 +58,10 @@ layout1 = html.Div([
         ]
     ),
     html.Div(id='app-1-display-value'),
-    dcc.Link('Go to App 2', href='/apps/app2')
+    dcc.Link('Go to App 2', href='/apps/app2'),
+    html.Div(id='app-1-display-value'),
+    dcc.Graph(figure = fig)
+
 ])
 
 layout2 = html.Div([
