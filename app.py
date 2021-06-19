4c11b602536b2ce904283975fbee50b87f9582ab
Gillestch <82659923+Gillestch@users.noreply.github.com>
2021-06-19 17:48:01 +0200

    5 graphs texte et menu

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ import dash
 
 
 
-app = dash.Dash(__name__, suppress_callback_exceptions=False, title ="Effet du Covid19 sur les infirmières")
+app = dash.Dash(__name__, suppress_callback_exceptions=False, title ="Effect of Covid 19 on the economy")
 server = app.server
